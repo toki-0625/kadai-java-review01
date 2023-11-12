389fe7eddf7731f8f2e2e5f1c90a46b414d165c6
toki-0625 <tokiaya0625@gmail.com>
2023-11-12 22:44:49 +0900

    再提出

--- src/Review01.java
+++ src/Review01.java
@@ -2,7 +2,7 @@ public class Review01 {
 
     public static void main(String[] args) {
         int productPrice = 1500;    //商品価格（税別）を入力
-        int tax = 10;               //消費税率（％）を入力
+        int tax = 51;               //消費税率（％）を入力
         int taxPrice;               //関数で消費税額の宣言
         taxPrice = tax(productPrice, tax);
         int taxIncludedPrice = productPrice + taxPrice;     //税込金額を商品価格と消費税額の合計から求める
@@ -10,7 +10,7 @@ public class Review01 {
     }
 
     public static int tax(int productPrice, int tax) {
-        int taxPrice = productPrice / tax;      //消費税額の計算
+        int taxPrice = productPrice / tax / 100;      //消費税額の計算
         return taxPrice;
     }
 
