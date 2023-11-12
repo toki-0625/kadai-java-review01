bc63b52ba3a1f69be87355512fcb191c8bfc2464
toki-0625 <tokiaya0625@gmail.com>
2023-11-12 21:40:58 +0900

    Signed-off-by: toki-0625 <tokiaya0625@gmail.com>

---
+++ src/Review01.java
@@ -0,0 +1,17 @@
+public class Review01 {
+
+    public static void main(String[] args) {
+        int productPrice = 1500;    //商品価格（税別）を入力
+        int tax = 10;               //消費税率（％）を入力
+        int taxPrice;               //関数で消費税額の宣言
+        taxPrice = tax(productPrice, tax);
+        int taxIncludedPrice = productPrice + taxPrice;     //税込金額を商品価格と消費税額の合計から求める
+        System.out.println(productPrice + "円の商品の税込価格は" + taxIncludedPrice + "円(消費税は" + taxPrice + "円です)");
+    }
+
+    public static int tax(int productPrice, int tax) {
+        int taxPrice = productPrice / tax;      //消費税額の計算
+        return taxPrice;
+    }
+
+}
